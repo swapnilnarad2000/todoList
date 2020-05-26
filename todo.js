c572e1c3474121c983880f8d49a28abedbe11612
swapnilnarad2000 <57103911+swapnilnarad2000@users.noreply.github.com>
2020-05-26 17:15:33 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,29 +1,18 @@
-//alert("connected");
-/*$("li").click(function(){
-	console.log("connected");
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		$(this).css({
-		color:"black",
-		textDecoration:"none"
-		});
-	}
-	else{
-		$(this).css({
-		color:"gray",
-		textDecoration:"line-through"
-		});
-		console.log($(this).css("color"))
-	}
-
-});
-*/
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("selected");
 });
 
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var todoItem= $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> "+todoItem+"</li>");
+	}
 });
