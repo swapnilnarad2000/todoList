da3eaa462c4e2fed7aef77106b44bf882fcb6b88
swapnilnarad2000 <57103911+swapnilnarad2000@users.noreply.github.com>
2020-05-28 00:12:33 +0530

    added styles

--- todo.js
+++ todo.js
@@ -1,3 +1,22 @@
+//alert("connected");
+/*$("li").click(function(){
+	console.log("connected");
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		$(this).css({
+		color:"black",
+		textDecoration:"none"
+		});
+	}
+	else{
+		$(this).css({
+		color:"gray",
+		textDecoration:"line-through"
+		});
+		console.log($(this).css("color"))
+	}
+
+});
+*/
 $("ul").on("click","li",function(){
 	$(this).toggleClass("selected");
 });
@@ -13,6 +32,11 @@ $("input[type='text']").keypress(function(event){
 	if(event.which===13){
 		var todoItem= $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span> "+todoItem+"</li>");
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+todoItem+"</li>");
 	}
+});
+
+$(".fa-plus").click(function(){
+	//$("input").toggleClass("display");
+	$("input[type='text']").fadeToggle();
 });
